3b3a8d1525d2e6992abad8da630b64bf67bc2c0b
daniel <daniel+github@sanitysoln.com>
2021-11-17 14:08:09 +0800

    mirgate src directory

--- examples/demo/src/views/Feedback.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <p>This is feedback page</p>
-  </div>
-</template>
--- examples/laravel/config/admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'url' => env('ADMIN_URL', 'http://localhost:8080'),
-    'read_only' => env('ADMIN_READ_ONLY', false),
-];
